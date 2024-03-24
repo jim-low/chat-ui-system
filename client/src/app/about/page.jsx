16cbed3772d5fb5454b2336baf0571d8540cbf9f
jim-low <robil12380@gmail.com>
2024-03-24 16:07:02 +0800

    Revert "feat: add server hosting that connects to frontend"
    This reverts commit 874f7f994aa60047ebef81b85a86614582d34875.

--- client/src/app/about/page.jsx
+++
@@ -1,9 +0,0 @@
-const About = () => {
-  return (
-    <h1>
-      haha About Us go brrr
-    </h1>
-  )
-}
-
-export default About
---
+++ client/src/app/page.js
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <div>haha NextJS go brr</div>
+  )
+}
