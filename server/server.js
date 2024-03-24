638b3b5bbb271858e644f11b6a838a4a381e4c4b
jim-low <robil12380@gmail.com>
2024-03-24 17:25:08 +0800

    feat: solve connection between backend and frontend

--- server/server.js
+++ server/server.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const PORT = 8080
-app.use(cors())
 
-app.get('/chat', (_, res) => {
-  res.json({ message: "Hello, World!", people: ["Harry", "Jack", "Barry"] })
+// middlewares
+app.use(cors()) // allow cross origin sharing
+app.use(express.json()) // decode request received from frontend
+
+app.post('/login', (request, response) => {
+})
+
+app.get('/chat', (request, response) => {
 })
 
 app.listen(PORT, () => {
