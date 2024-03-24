339a12150acb72a3bb3892bd8f4157440a67f86e
jim-low <robil12380@gmail.com>
2024-03-24 18:31:40 +0800

    fix: npm cache cleanup

--- client/src/app/login/page.jsx
+++ client/src/app/login/page.jsx
@@ -1,8 +1,12 @@
+"use client"
+
 import styles from './Login.module.css'
+import React from 'react'
 
 const Login = () => {
   return (
     <div id={styles.login}>
+      haha Login go brrrr
     </div>
   )
 }
