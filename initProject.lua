345e4197bae2291e8c08358bf28ab67bdb13b854
jim-low <robil12380@gmail.com>
2024-03-24 18:21:26 +0800

    chore: update init project script

--- initProject.lua
+++ initProject.lua
@@ -1,9 +1,9 @@
 local ui = require'harpoon.ui'
 local term = require'harpoon.term'
 
-ui.nav_file(2)
+ui.nav_file(1)
 term.gotoTerminal(1)
 term.sendCommand(1, "cd server && npm run dev\n")
 term.gotoTerminal(2)
 term.sendCommand(2, "cd client && npm run dev\n")
-ui.nav_file(2)
+ui.nav_file(1)
