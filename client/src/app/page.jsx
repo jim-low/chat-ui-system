f9ab8325017bf63bd85ceaefab0fa1ed1abb5578
jim-low <robil12380@gmail.com>
2024-03-29 09:38:07 +0800

    feat: reroute client to login page when loading home page

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,11 +1,11 @@
 "use client"
 
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 export default function Home() {
-  if (window.location.href === "http://localhost:3000" || window.location.href === "http://localhost:3000/") {
-    window.location.href = "http://localhost:3000/login"
-  }
+  const router = useRouter()
+  router.replace('/login')
 
   return (
     <div>Loading...</div>
