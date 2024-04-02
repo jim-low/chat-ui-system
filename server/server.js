61bf2a9cab18362aebff0b215d2c784f1259e051
jim-low <robil12380@gmail.com>
2024-04-02 21:26:11 +0800

    feat: serve file with backend server

--- server/server.js
+++ server/server.js
@@ -1,46 +1,9 @@
+const { join } = require('node:path')
 const express = require('express')
 const app = express()
-const cors = require('cors')
-const { default: axios } = require('axios')
-const PORT = 8080
+const PORT = 8000
 
-app.use(cors()) // allow cross origin sharing
-app.use(express.json()) // decode request received from frontend
-
-app.post('/login', (req, res) => {
-  const { username } = req.body
-  let found = false
-  let foundUser = {}
-
-  axios('http://13.212.255.177/api/chatSystem/users/list').then(response => {
-    const users = response.data
-    for (let i = 0; i < users.length; ++i) {
-      const currUser = users[i]
-      const splitName = currUser.username.split(" ")
-      if (currUser.username.includes(username) || splitName.includes(username)) {
-        foundUser = currUser.id
-        found = true
-        break
-      }
-    }
-    res.json({
-      found: found,
-      user: foundUser,
-    })
-  })
-})
-
-app.post('/user', (req, res) => {
-  const { userId } = req.body
-  axios(`http://13.212.255.177/api/chatSystem/user/${userId}`).then(response => {
-    res.json(response.data)
-  }).catch(err => {
-      console.error(err)
-      res.json({
-        message: err.data.error
-      })
-    })
-})
+app.use(express.static(join(__dirname, "build")))
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`)
