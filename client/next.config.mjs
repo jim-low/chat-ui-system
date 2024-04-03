2020e8c7ef66db0263c66a55d00a96fe2fef59ea
jim-low <robil12380@gmail.com>
2024-04-03 22:42:55 +0800

    fix: config for dynamically served pages

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export', // output: 'export' is for static exports, for statically hosted websites
   distDir: 'build',
   images: {
+    // domains: ["fastly.picsum.photos"],
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'picsum.photos'
+        hostname: 'fastly.picsum.photos',
+        port: '',
+        pathname: "/id/**"
       }
     ]
   }
