30c648eceb15ae32812fd95ca51603ec5bf1acac
jim-low <robil12380@gmail.com>
2024-04-03 22:42:33 +0800

    chore: change project init commands

--- initProject.lua
+++ initProject.lua
@@ -2,8 +2,8 @@ local ui = require'harpoon.ui'
 local term = require'harpoon.term'
 
 ui.nav_file(1)
+-- term.gotoTerminal(1)
+-- term.sendCommand(1, "cd server && brave http://localhost:8000 && npm run dev\n")
 term.gotoTerminal(1)
-term.sendCommand(1, "cd server && npm run dev\n")
-term.gotoTerminal(2)
-term.sendCommand(2, "cd client && brave http://localhost:3000 && npm run dev\n")
+term.sendCommand(1, "cd client && brave http://localhost:3000 && npm run dev\n")
 ui.nav_file(1)
