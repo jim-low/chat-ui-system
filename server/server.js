841b95ae005b45df5fb2773476e0ec813a529837
jim-low <robil12380@gmail.com>
2024-04-04 21:57:05 +0800

    chore: remove commented code

--- server/server.js
+++ server/server.js
@@ -3,8 +3,6 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
-app.use(express.static(join(__dirname, "build")))
-
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`)
 })
